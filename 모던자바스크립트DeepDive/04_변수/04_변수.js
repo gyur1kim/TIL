7bccd8ec0767375e7c94c27104f2c9e2b53e0086
gyur1kim <qri1114@gmail.com>
2023-07-10 23:53:28 +0900

    :sparkles: 04_변수
    - 4-5. 값의 할당

--- 모던자바스크립트DeepDive/04_변수/04_변수.js
+++ 모던자바스크립트DeepDive/04_변수/04_변수.js
@@ -12,3 +12,18 @@ let result = 10 + 20;
 // 4.4 변수 선언의 실행 시점과 변수 호이스팅
 console.log(score);  // undefined
 var score;           // 변수 선언문
+
+
+// 4-5. 값의 할당
+console.log(score1)   // undefined (아직 값이 할당되지 않음, 변수 선언만 실행)
+var score1;           // 변수 선언
+score1 = 80;          // 값의 할당
+console.log(score1)   // 값의 할당 실행됨, 80 출력
+
+console.log('==========문제!=========')
+console.log('====아래의 실행 결과는?====')
+
+console.log(ans);  // undefined
+ans = 80;
+var ans;
+console.log(ans)   // 80
