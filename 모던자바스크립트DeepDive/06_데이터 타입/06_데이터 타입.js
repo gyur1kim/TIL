80150f38530a9b9066c1626a0ab6c5aa5f8d3c5b
gyur1kim <qri1114@gmail.com>
2023-07-15 23:40:47 +0900

    :sparkles: 06_데이터 타입
    - 숫자 타입

---
+++ 모던자바스크립트DeepDive/06_데이터 타입/06_데이터 타입.js
@@ -0,0 +1,11 @@
+// 6.1 숫자 타입
+console.log(1 === 1.0)
+
+let bin = 0b01000001;
+let oct = 0o101;
+let hex = 0x41;
+console.log(bin);   // 모두 65 출력
+console.log(oct);
+console.log(hex);
+console.log(bin === oct);  // 모두 true
+console.log(oct === hex);
